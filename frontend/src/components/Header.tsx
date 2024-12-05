95c1f87608dd11235fddabc17313fb52da210f43
Dat Do <duy.t.d.do@stud.h-da.de>
2024-12-05 06:14:57 +0100

    fix: adjust test to ui change

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,12 +3,10 @@ import Typography from '@mui/material/Typography';
 
 export const Header = () => {
   return (
-    <header>
-      <AppBar sx={{ padding: '0.5rem 1rem' }}>
-        <Typography variant='h6' sx={{ flexGrow: 1 }}>
-          Free Parking
-        </Typography>
-      </AppBar>
-    </header>
+    <AppBar sx={{ padding: '0.5rem 1rem' }}>
+      <Typography variant='h6' sx={{ flexGrow: 1 }}>
+        Free Parking
+      </Typography>
+    </AppBar>
   );
 };
