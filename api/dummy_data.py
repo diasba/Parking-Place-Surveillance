99615833b810aa8415ad6218db6006e9a469eab4
Bogdan Polskiy <bogdan.polskiy03@gmail.com>
2024-12-18 12:15:29 +0100

    refactor: remove hardcoded dummy data from create_dummy_data function

--- api/dummy_data.py
+++ api/dummy_data.py
@@ -3,12 +3,7 @@ from parking_spot_status import ParkingSpotStatus
 
 def create_dummy_data():
     data_communicator = DataCommunicator()
-    dummy_data = [
-        ParkingSpotStatus(id=1, occupied=False, prediction="02:30:00"),
-        ParkingSpotStatus(id=2, occupied=True, prediction="04:00:00"),
-        ParkingSpotStatus(id=3, occupied=False, prediction="01:00:00"),
-    ]
-    data_communicator.update_data(dummy_data)
+
     return data_communicator
 
 if __name__ == "__main__":
