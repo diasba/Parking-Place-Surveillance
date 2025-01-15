48f1b894fa8b865e0ed380908f516548e9efc196
Jaro <jaroslav.gall332@gmail.com>
2025-01-15 20:59:35 +0100

    fix: tests running per default

--- tests/app_tests/test_app.py
+++ tests/app_tests/test_app.py
@@ -1,11 +1,11 @@
 import pytest
 
 class Test_Application:
-    def test_init():
+    def test_init(self):
         pass
     
-    def test_run_cycle():
+    def test_run_cycle(self):
         pass
     
-    def test_start():
+    def test_start(self):
         pass
--- tests/controller_tests/test_data_communicator.py
+++ tests/controller_tests/test_data_communicator.py
@@ -1,11 +1,11 @@
 import pytest
 
 class Test_Data_Communicator:
-    def test_init():
+    def test_init(self):
         pass
     
-    def test_update_data():
+    def test_update_data(self):
         pass
     
-    def test_get_data():
+    def test_get_data(self):
         pass
