1456992706c7832b10a759296e85b90b32cea853
Daniel NukeBayern <kay@nuke.bayern>
2025-02-04 00:11:33 +0100

    pi dev

--- processing/dummy_image_processor.py
+++ processing/dummy_image_processor.py
@@ -5,13 +5,13 @@ import numpy as np
 from datetime import datetime
 from processing.image_processor import ImageProcessor
 from controller.camera_controller import CameraController
-from processing.opencv_image_processor import OpenCVImageProcessor
+# from processing.opencv_image_processor import OpenCVImageProcessor
 
 
 class DummyImageProcessor(ImageProcessor):
     def __init__(self):
         self.camera_controller = CameraController()
-        self.opencv_processor = OpenCVImageProcessor()
+        # self.opencv_processor = OpenCVImageProcessor()
         self.start_saving_images()
 
     def process_image(self, image) -> json:
@@ -23,14 +23,14 @@ class DummyImageProcessor(ImageProcessor):
             image = self.camera_controller.get_current_image()
             if image:
                 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-                image.save(f"/home/{timestamp}.jpg")
+                image.save(f"/home/whoami/{timestamp}.jpg")
 
                 # Process the image using OpenCVImageProcessor
                 image_np = np.array(image)
-                processed_json = self.opencv_processor.process_image(image_np)
+                # processed_json = self.opencv_processor.process_image(image_np)
 
                 # Save the JSON output
-                with open(f"/home/{timestamp}.json", 'w') as json_file:
+                with open(f"/home/whoami/{timestamp}.json", 'w') as json_file:
                     json_file.write(processed_json)
 
             time.sleep(10)
